64bb46050acb291d52228d9359085bf3424e2294
GBC <gbc@semrum.ru>
2024-12-07 02:08:06 +0300

    Сделанна базовая задача + Графические улучшения

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Привязал UI
+    
+    @IBOutlet weak var background: UIImageView!
+        
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBOutlet weak var mainButton: UIButton!
+    
+    private var counterValue = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter.text = "0"
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        counterValue += 1
+        counter.text = String(counterValue)
+    }
+    
 }
 
