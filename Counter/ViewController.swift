1892489e947f586c1eb5b9e904112898826666d9
GBC <gbc@semrum.ru>
2024-12-07 15:35:47 +0300

    Усложнённая задача сделана

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -17,17 +18,52 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mainButton: UIButton!
     
-    private var counterValue = 0
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+        
+    @IBOutlet weak var history: UITextView!
+    
+   
+    private func currentTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter.string(from: Date())
+    }
+    
+    private var counterValue: UInt32 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         counter.text = "0"
+        history.isEditable = false
     }
 
     @IBAction func pressButton(_ sender: Any) {
         counterValue += 1
         counter.text = String(counterValue)
+        history.text += "\n\(currentTime()): Значение изменено на +1"
     }
+    
+    
+    @IBAction func pressMinusButton(_ sender: Any) {
+        if counterValue != 0 {counterValue -= 1
+            counter.text = String(counterValue)
+            history.text += "\n\(currentTime()): Значение изменено на -1"
+        }
+        else {
+            history.text += "\n\(currentTime()): Попытка уменьшить значение счётчика ниже 0"
+        }
+        
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        counterValue = 0
+        counter.text = String(counterValue)
+        history.text += "\n\(currentTime()): Значение сброшено"
+    }
+    
     
 }
 
