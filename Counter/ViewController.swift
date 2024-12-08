85d6b84195daeb5fb673c404b51303f395e2bb34
GBC <gbc@semrum.ru>
2024-12-08 12:14:49 +0300

    Добавлены модификаторы доступа

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,42 +12,42 @@ class ViewController: UIViewController {
 
     //Привязал UI
     
-    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak private var background: UIImageView!
         
-    @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak private var counter: UILabel!
     
-    @IBOutlet weak var mainButton: UIButton!
+    @IBOutlet weak private var mainButton: UIButton!
     
-    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak private var minusButton: UIButton!
     
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak private var resetButton: UIButton!
         
-    @IBOutlet weak var history: UITextView!
+    @IBOutlet weak private var history: UITextView!
     
    
+    private var counterValue: UInt32 = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        counter.text = "0"
+        history.isEditable = false
+    }
+
     private func currentTime() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         return dateFormatter.string(from: Date())
     }
     
-    private var counterValue: UInt32 = 0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        counter.text = "0"
-        history.isEditable = false
-    }
-
-    @IBAction func pressButton(_ sender: Any) {
+    @IBAction private func pressButton(_ sender: Any) {
         counterValue += 1
         counter.text = String(counterValue)
         history.text += "\n\(currentTime()): Значение изменено на +1"
     }
     
     
-    @IBAction func pressMinusButton(_ sender: Any) {
+    @IBAction private func pressMinusButton(_ sender: Any) {
         if counterValue != 0 {counterValue -= 1
             counter.text = String(counterValue)
             history.text += "\n\(currentTime()): Значение изменено на -1"
@@ -58,7 +58,7 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func resetButton(_ sender: Any) {
+    @IBAction private func resetButton(_ sender: Any) {
         counterValue = 0
         counter.text = String(counterValue)
         history.text += "\n\(currentTime()): Значение сброшено"
